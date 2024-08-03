5d9d47d1e9022558972e5290274a90ed33800cba
NQTrang1801 <trangnguyen180103@gmail.com>
2024-08-03 18:24:23 +0700

    add ex3: candy

---
+++ candy/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Solution {
+    public int Candy(int[] ratings) {
+        int n = ratings.Length;
+        int[] candys = new int[n];
+        candys[0] = 1;
+
+        // Duyệt từ trái sang phải
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candys[i] = candys[i - 1] + 1;
+            } else {
+                candys[i] = 1;
+            }
+        }
+
+        int sum = candys[n - 1];
+
+        // Duyệt từ phải sang trái
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candys[i] = Math.Max(candys[i], candys[i + 1] + 1);
+            }
+            sum += candys[i];
+        }
+
+        return sum;
+
+    }
+
+    public static void Main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 2, 87, 87, 87, 2, 1};
+        Console.WriteLine(solution.Candy(nums));
+
+    }
+}
